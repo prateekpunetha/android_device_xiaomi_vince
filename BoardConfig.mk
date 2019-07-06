694dd1d4f7f2dfb495779efbf36d46dbac5608dc
PrateekPunetha <prateekpunetha@gmail.com>
2019-07-06 12:14:15 +0000

    Set soong namespaces
    Signed-off-by: PrateekPunetha <prateekpunetha@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,16 @@ TARGET_COPY_OUT_VENDOR := vendor
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
+# Hals
+TARGET_QCOM_DISPLAY_VARIANT := caf-msm8996
+TARGET_QCOM_AUDIO_VARIANT := caf-msm8996
+TARGET_QCOM_MEDIA_VARIANT := caf-msm8996
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/qcom/display-$(TARGET_QCOM_DISPLAY_VARIANT) \
+    hardware/qcom/audio-$(TARGET_QCOM_AUDIO_VARIANT) \
+    hardware/qcom/media-$(TARGET_QCOM_MEDIA_VARIANT)
+
 # Kernel
 TARGET_KERNEL_CONFIG := vince_defconfig
 
